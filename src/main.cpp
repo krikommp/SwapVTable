0de0d0aad4f56caa16604de0c23eb7f3ec003c16
TaoZi <1136223096@qq.com>
2023-07-19 17:08:18 +0800

    支持 linux

--- src/main.cpp
+++ src/main.cpp
@@ -16,24 +16,21 @@ int main() {
 
     E *a = new E;
 
-    uint64_t offset = get_base_class_offset<E>(a);
-    printf("Base Offset: %lld\n", offset);
+    uint64_t offset = get_base_class_offset<B>(a);
+    printf("Base Offset: %ld\n", offset);
 
     auto [real_class_ptr, old_vtable] = get_class_and_vtable((char *) a, offset);
+    printf("old vtable address: %p\n", old_vtable);
 
     uint16_t v_count = get_v_fun_count(old_vtable);
     printf("vfunc count: %d\n", v_count);
 
     VFunMap map = {
-            {0, (uintptr_t) g_func}
+            {0, (uintptr_t) g_func},
+            //{1, (uintptr_t) g_func}
     };
 
     uintptr_t *new_vtable = swap_vtable(real_class_ptr, old_vtable, v_count, map);
-
-    printf("========== old vtable ==========\n");
-    foreach_call_vtable(real_class_ptr, old_vtable, v_count);
-    printf("========== new vtable ==========\n");
-    foreach_call_vtable(real_class_ptr, new_vtable, v_count);
 
     printf("========== A ==========\n");
     a->func_A_0();
@@ -43,7 +40,7 @@ int main() {
     a->func_B_0();
     a->func_B_1();
     a->func_B_2();
-    printf("========== E ==========\n");
+    printf("========== C ==========\n");
     a->func_E_0();
     a->func_E_1();
     a->func_E_2();
